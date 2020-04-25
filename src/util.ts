5b6616256e50a0cecdb1041ee7052c0c203e06d4
Nick Barker <cravingsleep@gmail.com>
2020-04-25 18:28:46 +0100

    First commit.

---
+++ src/util.ts
@@ -0,0 +1,15 @@
+// export function range(upto: number): number[] {
+//     return [...Array(upto).keys()];
+// }
+
+// export function rand(min: number, max: number): number { // min and max included 
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// export function randChoice<T>(arr: T[]): T {
+//     return arr[rand(0, arr.length - 1)];
+// }
+
+// function randFloat(min: number, max: number): number {
+//     return Math.random() < 0.5 ? ((1 - Math.random()) * (max - min) + min) : (Math.random() * (max - min) + min);
+// }
